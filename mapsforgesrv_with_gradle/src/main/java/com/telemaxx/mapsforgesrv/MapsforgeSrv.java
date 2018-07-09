46b63404a5ba492d7cf1d046f239bc780a99014f
telemaxx <6745th@web.de>
2018-07-09 18:26:13 +0200

    changing cli. -m mapfile -t themefile

--- mapsforgesrv_with_gradle/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
+++ mapsforgesrv_with_gradle/src/main/java/com/telemaxx/mapsforgesrv/MapsforgeSrv.java
@@ -9,19 +9,49 @@ public class MapsforgeSrv {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("MapsforgeSrv - a mapsforge tile server");
-		if (args.length < 1)
-			help();
+		
+		String mapFilePath = null;
+		String themeFilePath = null;
+		
+		Options options = new Options();
+		Option mapfileArgument = new Option("m", "mapfile", true, "mapsforge map file(.map)");
+	    mapfileArgument.setRequired(true);
+	    options.addOption(mapfileArgument);
+		
+        Option themefileArgument = new Option("t", "themefile", true, "mapsforge theme file(.xml)");
+        themefileArgument.setRequired(false);
+        options.addOption(themefileArgument);	
+        
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;      
 
-		File mapFile = new File(args[0]);
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("mapsforgesrv", options);
+            System.exit(1);
+        }
+        
+        mapFilePath = cmd.getOptionValue("mapfile").trim();
+        
+        themeFilePath = cmd.getOptionValue("themefile");
+        if (themeFilePath != null) {
+        	themeFilePath = themeFilePath.trim();
+        }
+        
+        File mapFile = new File(mapFilePath);
 		System.out.println("Map file: " + mapFile);
+		
 		if (!mapFile.isFile()) {
 			System.err.println("ERROR: Map file does not exist!");
 			System.exit(1);
 		}
 
 		File themeFile = null;
-		if (args.length > 1) {
-			themeFile = new File(args[1]);
+		if (themeFilePath != null) {	
+			themeFile = new File(themeFilePath);
 			System.out.println("Render theme file: " + themeFile);
 			if (!themeFile.isFile()) {
 				System.err.println("ERROR: Render theme file does not exist!");
@@ -45,8 +75,5 @@ public class MapsforgeSrv {
 		server.join();
 	}
 
-	public static void help() {
-		System.out.println("java -jar MapsforgeSrv.jar <mapfile> [themefile]");
-		System.exit(1);
-	}
+
 }
