0bc0407950e0ed9452eddfce3676fa17e3cc9c97
telemaxx <6745th@web.de>
2018-07-10 19:12:07 +0200

    new startscript

--- mapsforgesrv_with_gradle/jars_ready2use/Cycle.cmd
+++ mapsforgesrv_with_gradle/jars_ready2use/Cycle.cmd
@@ -1,5 +1,5 @@
 @Echo off
 rem cd /d C:\Users\top\BTSync\Exchange\gps_tools\MapsforgeSrv
 rem java.exe -jar MapsforgeSrv.jar  "..\..\..\oruxmaps\Mapsforge\Germany_ML.map" "Mapsforge\ELV_320\Elevelo_Cycling_L.xml"
-java.exe -jar C:\Users\top\git\mapsforgesrv_with_gradle\jars_ready2use\MapsforgeSrv.jar -m "C:\Users\top\BTSync\oruxmaps\mapfiles\Germany_ML.map" -t "C:\Users\top\BTSync\oruxmaps\mapstyles\ELV\Elements.xml" -p 8080
+java.exe -jar C:\Users\top\git\mapsforgesrv_with_gradle\jars_ready2use\MapsforgeSrv.jar -m "C:\Users\top\BTSync\oruxmaps\mapfiles\Germany_ML.map" -t "C:\Users\top\BTSync\oruxmaps\mapstyles\ELV\Elements.xml" -p 8080 -if "all"
 echo.
