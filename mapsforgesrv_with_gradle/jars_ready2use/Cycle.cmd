f6d9561b6476877ff35a337d0532f815545ab734
Thomas Th <6745th@web.de>
2021-03-01 19:22:07 +0100

    restored from backup

---
+++ mapsforgesrv_with_gradle/jars_ready2use/Cycle.cmd
@@ -0,0 +1,4 @@
+@Echo off
+java.exe -jar C:\Users\top\git\mapsforgesrv_with_gradle\mapsforgesrv_with_gradle\jars_ready2use\MapsforgeSrv.jar -r "direct" -m "C:\Users\top\BTSync\oruxmaps\mapfiles\Germany_V3_oam.map"  -t "C:\Users\top\BTSync\oruxmaps\mapstyles\ELV4\Elements.xml" -p 8080 -if "all"  -l "EN" -s "elmt-cycling"
+echo
+pause
